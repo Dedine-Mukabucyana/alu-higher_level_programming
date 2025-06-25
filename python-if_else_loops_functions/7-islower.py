800f0db429ae1a69e6d7401c5bf96d84dd4ce570
Dedine-Mukabucyana <d.mukabucya@alustudent.com>
2025-06-25 09:30:28 -0700

    slower

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def islower(c):
+    """Check if a character is lowercase."""
+    return ord('a') <= ord(c) <= ord('z')
+
+# Test cases
+if __name__ == "__main__":
+    print("a is {}".format("lower" if islower("a") else "upper"))
+    print("H is {}".format("lower" if islower("H") else "upper"))
+    print("A is {}".format("lower" if islower("A") else "upper"))
+    print("3 is {}".format("lower" if islower("3") else "upper"))
+    print("g is {}".format("lower" if islower("g") else "upper"))
