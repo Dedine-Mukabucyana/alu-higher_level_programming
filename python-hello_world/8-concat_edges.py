b09fa80ea11262c21c3e2410ce2cb3d21bc8994b
Dedine-Mukabucyana <d.mukabucya@alustudent.com>
2025-06-25 10:33:39 -0700

    edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-
-def uppercase(str):
-    """Print a string in uppercase without using str.upper()."""
-    for char in str:
-        diff = 0
-        if ord('a') <= ord(char) <= ord('z'):
-            diff = 32
-        print("{:c}".format(ord(char) - diff), end="")
-    print()
-
-
-if __name__ == "__main__":
-    uppercase("holberton")  # HOLBERTON
-    uppercase("Holberton School")  # HOLBERTON SCHOOL
-    uppercase("Holberton School, 98 battery street")  # HOLBERTON SCHOOL, 98 BATTERY STREET
-    uppercase("")  # (prints just newline)
-    uppercase("z")  :
+print(__import__('sys').stdout.write(
+    str().join([chr(111), chr(98), chr(106), chr(101), chr(99), chr(116), chr(45),
+    chr(111), chr(114), chr(105), chr(101), chr(110), chr(116), chr(101), chr(100),
+    chr(32), chr(112), chr(114), chr(111), chr(103), chr(114), chr(97), chr(109),
+    chr(109), chr(105), chr(110), chr(103), chr(32), chr(119), chr(105), chr(116),
+    chr(104), chr(32), chr(80), chr(121), chr(116), chr(104), chr(111), chr(110)])))
