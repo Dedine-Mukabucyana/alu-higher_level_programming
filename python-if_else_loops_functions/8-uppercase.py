87127a1271e0179976ff5904cba388ce97debcc0
Dedine-Mukabucyana <d.mukabucya@alustudent.com>
2025-06-25 10:01:14 -0700

    upper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def uppercase(str):
     for c in str:
         if 97 <= ord(c) <= 122:  # a-z
@@ -5,4 +6,3 @@ def uppercase(str):
         else:
             print("{}".format(c), end='')
     print()
-
