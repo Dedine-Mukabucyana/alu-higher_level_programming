fd542d462a360d77cd6d52cb1ef435e06f795a15
Dedine-Mukabucyana <d.mukabucya@alustudent.com>
2025-06-25 10:31:41 -0700

    edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[106:112] + str[:6])
+
+def uppercase(str):
+    """Print a string in uppercase without using str.upper()."""
+    for char in str:
+        diff = 0
+        if ord('a') <= ord(char) <= ord('z'):
+            diff = 32
+        print("{:c}".format(ord(char) - diff), end="")
+    print()
+
+
+if __name__ == "__main__":
+    uppercase("holberton")  # HOLBERTON
+    uppercase("Holberton School")  # HOLBERTON SCHOOL
+    uppercase("Holberton School, 98 battery street")  # HOLBERTON SCHOOL, 98 BATTERY STREET
+    uppercase("")  # (prints just newline)
+    uppercase("z")  :
