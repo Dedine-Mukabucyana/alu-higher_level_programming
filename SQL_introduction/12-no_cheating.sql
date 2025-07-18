a6c847abf4ca1cd0dc4e2aa86d26330badd51471
Dedine-Mukabucyana <d.mukabucya@alustudent.com>
2025-07-18 04:56:32 -0700

    sql

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,9 @@
---  list all records with a score >= 10.
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- Update data in table; change Bob's score to 10
+--
+-- score   name
+-- 10  Bob
+-- 10  John
+-- 8   George
+-- 3   Alex
+
+UPDATE second_table SET `score` = 10 WHERE `name` = 'Bob';
