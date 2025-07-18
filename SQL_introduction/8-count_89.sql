e096949a7a9b8ab0d917ca493e419a5293ac598c
Dedine-Mukabucyana <d.mukabucya@alustudent.com>
2025-07-18 03:38:21 -0700

    assignments

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,8 @@
--- Script to update Bob's score to 10 in second_table
+-- This script updates the score of all records with name 'Bob' to 10
+-- It handles cases with zero, one, or multiple Bob records
+-- It doesn't use Bob's id value, only the name field
+-- SQL keywords are in uppercase as required
+
 UPDATE second_table
 SET score = 10
 WHERE name = 'Bob';
